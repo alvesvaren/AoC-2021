11e873f8a80477522333608f5fb66467c085d7e5
Alve Svarén <alve@hotmail.se>
2021-12-01 09:44:42 +0100

    Solution for day 1

--- 01.py
+++ 01.py
@@ -1,5 +1,22 @@
 from aoc import get_input
 
-data = get_input(1).splitlines()
+data = *map(int, get_input(1).splitlines()),
 
-print(data)
+def calculate_larger(data):
+    count = 0
+    last_item = None
+    for item in data:
+        if last_item is not None and item > last_item:
+            count += 1
+        last_item = item
+    return count
+
+new_data = []
+
+for first, second, third in zip(data, data[1:], data[2:]):
+    new_data.append(first + second + third)
+
+
+
+print("Part 1:", calculate_larger(data))
+print("Part 2:", calculate_larger(new_data))

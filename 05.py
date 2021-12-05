ec9bc234c37e470f54623182bfc05092d07e03a6
Alve Svarén <alve@hotmail.se>
2021-12-05 18:16:38 +0100

    Clean up solution for day 5 even more

--- 05.py
+++ 05.py
@@ -1,5 +1,5 @@
 import aoc
-from itertools import chain
+import re
 from collections import defaultdict
 
 data = aoc.get_input(5).splitlines()
@@ -8,8 +8,8 @@ normal_vents = defaultdict(int)
 all_vents = defaultdict(int)
 
 for line in data:
-    start, end = line.split(" -> ")
-    x1, y1, x2, y2 = map(int, chain(start.split(","), end.split(",")))
+    assert (match := re.match(r"(\d+),(\d+) -> (\d+),(\d+)", line))
+    x1, y1, x2, y2 = map(int, match.groups())
 
     if (x1 == x2) or (y1 == y2):
         for x in range(min(x1, x2), max(x1, x2)+1):
@@ -20,9 +20,5 @@ for line in data:
         for ix, x in enumerate(range(x1, x2+1) if x1 <= x2 else range(x1, x2-1, -1)):
             all_vents[(x, y1 + (ix if y2 > y1 else -ix))] += 1
 
-
-count1 = sum(count >= 2 for count in normal_vents.values())
-count2 = sum(count >= 2 for count in all_vents.values())
-
-print("Part 1:", count1)
-print("Part 2:", count2)
+print("Part 1:", sum(count >= 2 for count in normal_vents.values()))
+print("Part 2:", sum(count >= 2 for count in all_vents.values()))
